d4d9a843a1038a3935f2c9993d5bfb43d55fe5aa
Bella Rose Wilson <bwilson9893@gmail.com>
2025-09-30 18:37:48 +0000

    all fixed except for eldest

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get("/movies/:path_id", { :controller => "movies", :action => "show" })  
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
   get("/directors/oldest", { :controller => "directors", :action => "oldest" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
 end
